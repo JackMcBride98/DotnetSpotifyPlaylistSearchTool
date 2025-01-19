a53829bf20056312ab2d57c5726801436f0aa210
Jack McBride <mcbride.jack1@gmail.com>
2025-01-19 14:12:01 +0000

    Scaffold login endpoint

--- Features/LogIn.cs
+++ Features/LogIn.cs
@@ -1,6 +1,40 @@
+using FastEndpoints;
+using SpotifyAPI.Web;
+
 namespace DotnetSpotifyPlaylistSearchTool.Features;
 
 public class LogIn
 {
-    
+    public record Response(Uri LoginUri);
+    public class Endpoint(IConfiguration config) : EndpointWithoutRequest<EmptyResponse>
+    {
+        public override void Configure()
+        {
+            Post("/login");
+            AllowAnonymous();
+        }
+        
+        public override async Task<Response> HandleAsync(CancellationToken cancellationToken)
+        {
+            var spotifyClientId = config.GetValue<string>("Spotify:ClientId");
+            if (string.IsNullOrWhiteSpace(spotifyClientId))
+            {
+                ThrowError("Spotify client id is missing");
+            }
+
+            var loginRequest = new LoginRequest(new Uri("http://localhost:5030"), spotifyClientId,
+                LoginRequest.ResponseType.Code)
+            {
+                Scope =
+                [
+                    Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative, Scopes.UserReadPrivate,
+                    Scopes.UserReadEmail
+                ]
+            };
+            
+            var uri = loginRequest.ToUri();
+
+            return new Response(uri);
+        }
+    }
 }
