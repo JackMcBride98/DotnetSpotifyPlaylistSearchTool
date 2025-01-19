816f39f05aa60df95428e870fd61d413e675cfd3
Jack McBride <mcbride.jack1@gmail.com>
2025-01-19 15:58:45 +0000

    Create react-vite-ts template app with tailwind and prettier

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center">
+      <h1 className="font-bold text-5xl">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
