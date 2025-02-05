c8f7b7f2a8b3d5897ed192dcfca4e51d7a1f7a10
Jack McBride <jack.mcbride@ghyston.com>
2025-02-05 09:17:20 +0000

    Tweak search bar button to increase its clickable area, type response

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -2,11 +2,34 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import searchIcon from "../assets/search.svg";
 import { useQuery } from "@tanstack/react-query";
+import { SpinnerCircularFixed } from "spinners-react";
+
+type Response = {
+  MatchingPlaylists: PlaylistResponse[];
+};
+
+type PlaylistResponse = {
+  Name: string;
+  OwnerName: string;
+  Image: ImageResponse;
+  Tracks: TrackResponse[];
+};
+
+type TrackResponse = {
+  Name: string;
+  ArtistName: string;
+  Match: boolean;
+};
+
+type ImageResponse = {
+  Url: string;
+};
+
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [showOnlyOwnPlaylists, setShowOnlyOwnPlaylists] = useState(false);
 
-  const { refetch } = useQuery({
+  const { refetch, isLoading, isError, error } = useQuery<Response, Error>({
     queryKey: ["search", searchTerm, showOnlyOwnPlaylists],
     queryFn: async () => {
       const res = await fetch(
@@ -22,14 +45,16 @@ export const Search = () => {
   });
 
   const handleSearch = () => {
-    void refetch();
+    if (searchTerm) {
+      void refetch();
+    }
   };
 
   return (
     <>
       <motion.div
         whileTap={{ scale: 0.9 }}
-        className="md:w-80 w-72 flex items-center space-x-2 bg-green-600 rounded-md pr-2 p-1"
+        className="md:w-80 w-72 flex items-center bg-green-600 rounded-md pl-1"
       >
         <input
           className="p-2 outline-0 bg-black w-full overflow-visible"
@@ -43,16 +68,20 @@ export const Search = () => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className=" p-2" onClick={handleSearch}>
+        <button
+          disabled={isLoading}
+          className="p-4 disabled:cursor-not-allowed"
+          onClick={handleSearch}
+        >
           <img
             src={searchIcon}
             alt="search"
-            className="w-5 h-5 fill-black bg-green-600"
+            className="w-6 h-6 fill-black bg-green-600"
           />
         </button>
       </motion.div>
       <label className="flex items-center">
-        Show only your made playlists
+        Show only your own playlists
         <input
           type="checkbox"
           className="accent-green-600 w-4 h-4 ml-2"
@@ -62,6 +91,8 @@ export const Search = () => {
           }}
         />
       </label>
+      {isLoading && <SpinnerCircularFixed />}
+      {isError && <p className="text-red-600">Error: {error?.message}</p>}
     </>
   );
 };
