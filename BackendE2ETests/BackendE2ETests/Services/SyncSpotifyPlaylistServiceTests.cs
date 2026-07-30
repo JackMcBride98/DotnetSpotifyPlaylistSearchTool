269dc8d46165c2d0b2b3cf459e4c7d1abf19ad4f
Jack McBride <mcbride.jack1@gmail.com>
2026-07-30 15:09:09 +0100

    Clean up tests and add 2 more

--- BackendE2ETests/BackendE2ETests/Services/SyncSpotifyPlaylistServiceTests.cs
+++ BackendE2ETests/BackendE2ETests/Services/SyncSpotifyPlaylistServiceTests.cs
@@ -33,17 +33,7 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
         Db.Users.AddRange(activeUser1, activeUser2);
         await Db.SaveChangesAsync(ct);
 
-        var authServiceMock = Substitute.For<ISpotifyAuthService>();
-
-        var syncService = Substitute.ForPartsOf<SyncSpotifyPlaylistService>(Db, authServiceMock);
-
-        syncService
-            .SyncPlaylistsForUserAsync(
-                Arg.Any<string>(),
-                Arg.Any<bool>(),
-                Arg.Any<CancellationToken>()
-            )
-            .Returns(Task.CompletedTask);
+        var syncService = CreateSyncServiceSubstituteForSyncActiveUsers();
 
         // Act
         await syncService.SyncActiveUsersAsync(ct);
@@ -81,17 +71,7 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
         Db.Users.AddRange(nullActiveUser, oldUser1, oldUser2);
         await Db.SaveChangesAsync(ct);
 
-        var authServiceMock = Substitute.For<ISpotifyAuthService>();
-
-        var syncService = Substitute.ForPartsOf<SyncSpotifyPlaylistService>(Db, authServiceMock);
-
-        syncService
-            .SyncPlaylistsForUserAsync(
-                Arg.Any<string>(),
-                Arg.Any<bool>(),
-                Arg.Any<CancellationToken>()
-            )
-            .Returns(Task.CompletedTask);
+        var syncService = CreateSyncServiceSubstituteForSyncActiveUsers();
 
         // Act
         await syncService.SyncActiveUsersAsync(ct);
@@ -149,16 +129,7 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
         Db.Users.Add(user);
         await Db.SaveChangesAsync(ct);
 
-        var authServiceMock = Substitute.For<ISpotifyAuthService>();
-        authServiceMock
-            .GetSpotifyClientAsync(
-                Arg.Any<string>(),
-                Arg.Any<string>(),
-                Arg.Any<CancellationToken>()
-            )
-            .ThrowsAsync(new Exception(apiErrorMessage));
-
-        var syncService = new SyncSpotifyPlaylistService(Db, authServiceMock);
+        var syncService = CreateSyncServiceWithAuthServiceError(apiErrorMessage);
 
         // Act & Assert
         var exception = await Should.ThrowAsync<Exception>(async () =>
@@ -196,16 +167,7 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
         Db.Users.Add(user);
         await Db.SaveChangesAsync(ct);
 
-        var authServiceMock = Substitute.For<ISpotifyAuthService>();
-        authServiceMock
-            .GetSpotifyClientAsync(
-                Arg.Any<string>(),
-                Arg.Any<string>(),
-                Arg.Any<CancellationToken>()
-            )
-            .ThrowsAsync(new Exception(apiErrorMessage));
-
-        var syncService = new SyncSpotifyPlaylistService(Db, authServiceMock);
+        var syncService = CreateSyncServiceWithAuthServiceError(apiErrorMessage);
 
         // Act & Assert
         var exception = await Should.ThrowAsync<Exception>(async () =>
@@ -301,37 +263,7 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
             new() { Id = "playlist_3", Collaborative = true },
         };
 
-        var spotifyClientMock = Substitute.For<ISpotifyClient>();
-        var authServiceMock = Substitute.For<ISpotifyAuthService>();
-
-        authServiceMock
-            .GetSpotifyClientAsync(
-                Arg.Any<string>(),
-                Arg.Any<string>(),
-                Arg.Any<CancellationToken>()
-            )
-            .Returns(Task.FromResult(spotifyClientMock));
-
-        var syncService = Substitute.ForPartsOf<SyncSpotifyPlaylistService>(Db, authServiceMock);
-
-        syncService
-            .GetAllCurrentUsersPlaylistsAsync(
-                Arg.Any<ISpotifyClient>(),
-                Arg.Any<CancellationToken>()
-            )
-            .Returns(Task.FromResult<IList<FullPlaylist>>(mockPlaylists));
-
-        syncService
-            .GetAllPlaylistTracksAsync(
-                Arg.Any<ISpotifyClient>(),
-                Arg.Any<string>(),
-                Arg.Any<CancellationToken>()
-            )
-            .Returns(
-                Task.FromResult<IList<PlaylistTrack<IPlayableItem>>>(
-                    new List<PlaylistTrack<IPlayableItem>>()
-                )
-            );
+        var syncService = CreateSyncServiceWithMockedPlaylists(mockPlaylists);
 
         // Act
         await syncService.SyncPlaylistsForUserAsync(
@@ -348,16 +280,209 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
     }
 
     [Fact]
-    public async Task SyncPlaylistsForUser_PlaylistsReturned_OnlySyncsOwnedOrCollaborativePlaylists() { }
+    public async Task SyncPlaylistsForUser_PlaylistsReturned_OnlySyncsOwnedOrCollaborativePlaylists()
+    {
+        // Arrange
+        var ct = TestContext.Current.CancellationToken;
+        const string targetUserId = "target_user_id";
+        const string otherUserId = "other_user_id";
+
+        var user = new UserBuilder { UserId = targetUserId }.Build();
+
+        Db.Users.Add(user);
+        await Db.SaveChangesAsync(ct);
+
+        var mockPlaylists = new List<FullPlaylist>
+        {
+            new()
+            {
+                Id = "owned_playlist_1",
+                Owner = new PublicUser { Id = targetUserId },
+                Collaborative = false,
+            },
+            new()
+            {
+                Id = "owned_playlist_2",
+                Owner = new PublicUser { Id = targetUserId },
+                Collaborative = false,
+            },
+            new()
+            {
+                Id = "collaborative_playlist",
+                Owner = new PublicUser { Id = otherUserId },
+                Collaborative = true,
+            },
+            new()
+            {
+                Id = "ignored_other_user_playlist",
+                Owner = new PublicUser { Id = otherUserId },
+                Collaborative = false,
+            },
+        };
+
+        var syncService = CreateSyncServiceWithMockedPlaylists(mockPlaylists);
+
+        // Act
+        await syncService.SyncPlaylistsForUserAsync(
+            targetUserId,
+            requiresProgressUpdates: false,
+            ct
+        );
+
+        // Assert
+        Db.ChangeTracker.Clear();
+
+        var savedPlaylists = await Db.Playlists.ToListAsync(ct);
+
+        savedPlaylists.Count.ShouldBe(3);
+        savedPlaylists
+            .Select(p => p.PlaylistId)
+            .ShouldBe(
+                new[] { "owned_playlist_1", "owned_playlist_2", "collaborative_playlist" },
+                ignoreOrder: true
+            );
+    }
 
     [Fact]
-    public async Task SyncPlaylistsForUser_RequiresProgressUpdates_SuccessfulSync_SyncStatusSetToCompleted() { }
+    public async Task SyncPlaylistsForUser_RequiresProgressUpdates_SuccessfulSync_SyncStatusSetToCompleted()
+    {
+        // Arrange
+        var ct = TestContext.Current.CancellationToken;
+        const string userId = "completed_status_user_id";
+
+        var user = new UserBuilder { UserId = userId }
+            .WithSyncState(SyncStatus.NotStarted)
+            .Build();
+
+        Db.Users.Add(user);
+        await Db.SaveChangesAsync(ct);
+
+        var mockPlaylists = new List<FullPlaylist>
+        {
+            new()
+            {
+                Id = "user_playlist_1",
+                Owner = new PublicUser { Id = userId },
+            },
+        };
+
+        var syncService = CreateSyncServiceWithMockedPlaylists(mockPlaylists);
+
+        // Act
+        await syncService.SyncPlaylistsForUserAsync(userId, requiresProgressUpdates: true, ct);
+
+        // Assert
+        Db.ChangeTracker.Clear();
+        var userInDb = await Db.Users.SingleAsync(u => u.UserId == userId, ct);
+
+        userInDb.SyncState.Status.ShouldBe(SyncStatus.Completed);
+        userInDb.SyncState.CompletedAt.ShouldNotBeNull();
+    }
 
     [Fact]
-    public async Task SyncPlaylistsForUser_SuccessfulSync_SyncCompletedAtGetsSet() { }
+    public async Task SyncPlaylistsForUser_SavesPlaylistWithTracksAndImages_PlaylistContainsBothEpisodesAndTracks_AsExpected()
+    {
+        // Arrange
+        var ct = TestContext.Current.CancellationToken;
+        const string userId = "world_class_dev_user_id";
+        const string playlistId = "poly_playlist_99";
+        const string snapshotId = "snapshot_v1_abc";
 
-    [Fact]
-    public async Task SyncPlaylistsForUser_SavesPlaylistWithTracksAndImages_PlaylistContainsBothEpisodesAndTracks_AsExpected() { }
+        var user = new UserBuilder { UserId = userId }.Build();
+        Db.Users.Add(user);
+        await Db.SaveChangesAsync(ct);
+
+        var mockTrack = new FullTrack
+        {
+            Type = ItemType.Track,
+            Name = "Midnight City",
+            Artists = new List<SimpleArtist>
+            {
+                new() { Name = "M83" },
+                new() { Name = "Anthony Gonzalez" },
+            },
+        };
+
+        var mockEpisode = new FullEpisode
+        {
+            Type = ItemType.Episode,
+            Name = "Episode 42: Deep Dive into EF Core",
+            Show = new SimpleShow { Name = "The C# Tech Podcast" },
+        };
+
+        var mockPlaylistTracks = new Paging<PlaylistTrack<IPlayableItem>>
+        {
+            Items = new List<PlaylistTrack<IPlayableItem>>
+            {
+                new() { Item = mockTrack },
+                new() { Item = mockEpisode },
+            },
+        };
+
+        var mockPlaylist = new FullPlaylist
+        {
+            Id = playlistId,
+            Name = "Synthwave & C# Podcasts",
+            Description = "The ultimate dev playlist",
+            Owner = new PublicUser { Id = userId, DisplayName = "Lead Architect" },
+            SnapshotId = snapshotId,
+            Collaborative = false,
+            Images = new List<SpotifyAPI.Web.Image>
+            {
+                new()
+                {
+                    Url = "https://cdn.spotify.com/images/playlist_cover.jpg",
+                    Width = 640,
+                    Height = 640,
+                },
+            },
+            Items = mockPlaylistTracks,
+        };
+
+        var syncService = CreateSyncServiceWithMockedPlaylists(
+            new List<FullPlaylist> { mockPlaylist }
+        );
+
+        // Act
+        await syncService.SyncPlaylistsForUserAsync(userId, requiresProgressUpdates: true, ct);
+
+        // Assert
+        Db.ChangeTracker.Clear();
+
+        var savedPlaylist = await Db
+            .Playlists.Include(p => p.Image)
+            .Include(p => p.Tracks)
+            .Include(p => p.Users)
+            .SingleOrDefaultAsync(p => p.PlaylistId == playlistId, ct);
+
+        savedPlaylist.ShouldNotBeNull();
+        savedPlaylist.Name.ShouldBe("Synthwave & C# Podcasts");
+        savedPlaylist.Description.ShouldBe("The ultimate dev playlist");
+        savedPlaylist.OwnerName.ShouldBe("Lead Architect");
+        savedPlaylist.SnapshotId.ShouldBe(snapshotId);
+
+        savedPlaylist.Image.ShouldNotBeNull();
+        savedPlaylist.Image.Url.ShouldBe("https://cdn.spotify.com/images/playlist_cover.jpg");
+        savedPlaylist.Image.Width.ShouldBe(640);
+        savedPlaylist.Image.Height.ShouldBe(640);
+
+        savedPlaylist.Users.ShouldHaveSingleItem();
+        savedPlaylist.Users!.First().UserId.ShouldBe(userId);
+
+        savedPlaylist.Tracks!.Count.ShouldBe(2);
+
+        var musicTrackEntity = savedPlaylist.Tracks.Single(t => t.Index == 0);
+        musicTrackEntity.Name.ShouldBe("Midnight City");
+        musicTrackEntity.ArtistName.ShouldBe("M83, Anthony Gonzalez");
+        musicTrackEntity.PlaylistId.ShouldBe(playlistId);
+
+        var podcastEpisodeEntity = savedPlaylist.Tracks.Single(t => t.Index == 1);
+        podcastEpisodeEntity.Name.ShouldBe(
+            "The C# Tech Podcast - Episode 42: Deep Dive into EF Core"
+        );
+        podcastEpisodeEntity.ArtistName.ShouldBe(string.Empty);
+        podcastEpisodeEntity.PlaylistId.ShouldBe(playlistId);
+    }
 
     [Fact]
     public async Task SyncPlaylistsForUser_PlaylistWithNullId_SkipsPlaylistWithoutCrashing() { }
@@ -376,4 +501,77 @@ public class SyncSpotifyPlaylistServiceTests(App app) : TestBase(app)
 
     [Fact]
     public async Task SyncPlaylistsForUser_ExistingPlaylistSnapshotIdsNotEqual_UpdatesPlaylistDetailsAndOverwritesTracks() { }
+
+    private SyncSpotifyPlaylistService CreateSyncServiceSubstituteForSyncActiveUsers()
+    {
+        var authServiceMock = Substitute.For<ISpotifyAuthService>();
+        var syncService = Substitute.ForPartsOf<SyncSpotifyPlaylistService>(Db, authServiceMock);
+
+        syncService
+            .SyncPlaylistsForUserAsync(
+                Arg.Any<string>(),
+                Arg.Any<bool>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.CompletedTask);
+
+        return syncService;
+    }
+
+    private SyncSpotifyPlaylistService CreateSyncServiceWithAuthServiceError(string errorMessage)
+    {
+        var authServiceMock = Substitute.For<ISpotifyAuthService>();
+        authServiceMock
+            .GetSpotifyClientAsync(
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<CancellationToken>()
+            )
+            .ThrowsAsync(new Exception(errorMessage));
+
+        return new SyncSpotifyPlaylistService(Db, authServiceMock);
+    }
+
+    private SyncSpotifyPlaylistService CreateSyncServiceWithMockedPlaylists(
+        IList<FullPlaylist> mockPlaylists
+    )
+    {
+        var spotifyClientMock = Substitute.For<ISpotifyClient>();
+        var authServiceMock = Substitute.For<ISpotifyAuthService>();
+
+        authServiceMock
+            .GetSpotifyClientAsync(
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromResult(spotifyClientMock));
+
+        var syncService = Substitute.ForPartsOf<SyncSpotifyPlaylistService>(Db, authServiceMock);
+
+        syncService
+            .GetAllCurrentUsersPlaylistsAsync(
+                Arg.Any<ISpotifyClient>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromResult(mockPlaylists));
+
+        syncService
+            .GetAllPlaylistTracksAsync(
+                Arg.Any<ISpotifyClient>(),
+                Arg.Any<string>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(callInfo =>
+            {
+                var playlistId = callInfo.ArgAt<string>(1);
+                var matchingPlaylist = mockPlaylists.FirstOrDefault(p => p.Id == playlistId);
+
+                IList<PlaylistTrack<IPlayableItem>> tracks = matchingPlaylist?.Items?.Items ?? [];
+
+                return Task.FromResult(tracks);
+            });
+
+        return syncService;
+    }
 }
