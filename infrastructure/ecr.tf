ff6eba1a29ba953e56a62401dcb691e94292a0e7
Jack McBride <37148941+JackMcBride98@users.noreply.github.com>
2026-08-10 11:23:29 +0100

    Create migrations ecr repo (#14)

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -7,6 +7,19 @@
   }
 }
 
+resource "aws_ecr_repository" "migrate" {
+  name                 = "playlist-search-tool-migrations"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 output "api_ecr_repository_url" {
   value = aws_ecr_repository.api.repository_url
+}
+
+output "migrate_ecr_repository_url" {
+  value = aws_ecr_repository.migrate.repository_url
 }
