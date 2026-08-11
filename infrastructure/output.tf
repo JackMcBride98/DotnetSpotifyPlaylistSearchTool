7d84743db82852ed37f81e813199cb3d9d1122fd
Jack McBride <mcbride.jack1@gmail.com>
2026-08-11 19:11:10 +0100

    Attempt fix by assigning task public ip and running it on the public subnets

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,5 +1,5 @@
-﻿output "private_subnet_ids" {
-  value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+﻿output "public_subnet_ids" {
+  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
 
 output "ecs_tasks_security_group_id" {
